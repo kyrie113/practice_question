1dde4b24454e55290102594228d274703837e258
“kyrie113 <137795459@qq.com>
2019-04-07 22:36:05 +0800

    第一个页面

---
+++ src/components/animation.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>sssssssssssssssssssssssssssssssssssssssssssssssss</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import main from '@/components/main'
+import animation from '@/components/animation'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'main',
+      component: main
+    },
+    {
+      path: '/animation',
+      name: 'animation',
+      component: animation
     }
   ]
 })
