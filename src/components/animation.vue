ccda5913b2c9966cc4a1ccde541acd96560c2d74
chenhuigui <chg@spacetech.com.cn>
2019-04-08 18:46:20 +0800

    文件设置

--- src/components/animation.vue
+++ src/components/animation.vue
@@ -22,21 +22,21 @@ export default {
     return {
       model: 1,
       banner: [
-        { id: 1, name: "推荐", url: "/animation" },
-        { id: 2, name: "MAD·AMV", url: "/animation/sonPage" },
-        { id: 3, name: "MMD·3D", url: "/" },
-        { id: 4, name: "短片·手书·配音", url: "/" },
-        { id: 5, name: "综合", url: "/" }
+        { id: 1, name: '推荐', url: '/animation' },
+        { id: 2, name: 'MAD·AMV', url: '/animation/sonPage' },
+        { id: 3, name: 'MMD·3D', url: '/' },
+        { id: 4, name: '短片·手书·配音', url: '/' },
+        { id: 5, name: '综合', url: '/' }
       ]
-    };
+    }
   },
   methods: {
     linkTo(data) {
       //   const url = this.$route.path;    有个路径判断问题， 这里不存在，子路由很多的时候存在拼接问题
-      this.$router.push(data.url);
+      this.$router.push(data.url)
     }
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .container {
