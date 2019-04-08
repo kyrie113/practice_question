901984bfa09678ec5d09c6385c436a7b462091ac
“kyrie113 <137795459@qq.com>
2019-04-08 22:59:35 +0800

    封装组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="app">
     <div class="back_color"></div>
-    <div class="top">
-      <div class="top_left"><img src="static/icon/topIcon.png"
-          alt=""></div>
-      <div class="top_search"><img src="static/icon/find.png"
-          alt=""
-          width="100%"
-          height="100%"></div>
-      <div class="top_user-icon"><img src="static/icon/userIcon.webp"
-          alt=""
-          width="100%"></div>
-      <button class="top-button">下载 App</button>
-    </div>
+    <top-banner></top-banner>
     <div class="head_line">
       <mt-navbar v-model="selected"
         class="nav_bar">
@@ -54,8 +43,13 @@
 
 <script>
 import { METHODS } from "http";
+import topBanner from "@/components/top-banner.vue"
 export default {
   name: "App",
+  components:{
+     topBanner,
+  },
+ 
   data() {
     return {
       popupVisible: false,
@@ -108,45 +102,45 @@ export default {
   z-index: 1000;
   background: #ffff;
 }
-.top {
-  position: fixed;
-  top: 0px;
-  z-index: 2500;
-  color: #ffff;
-  &_left {
-    position: absolute;
-    left: 14px;
-    top: 6.5px;
-  }
-  &_search {
-    position: absolute;
-    width: 24px;
-    height: 24px;
-    overflow: hidden;
-    left: 193px;
-    top: 9.5px;
-  }
-  &_user-icon {
-    position: absolute;
-    width: 24px;
-    height: 24px;
-    overflow: hidden;
-    left: 240px;
-    top: 6.5px;
-  }
-  &-button {
-    position: absolute;
-    width: 80px;
-    height: 25px;
-    border: solid #fb7299;
-    border-radius: 4px;
-    background-color: #fb7299;
-    font-size: 13px;
-    color: #ffff;
-    left: 275px;
-    top: 9.5px;
-  }
-}
+// .top {
+//   position: fixed;
+//   top: 0px;
+//   z-index: 2500;
+//   color: #ffff;
+//   &_left {
+//     position: absolute;
+//     left: 14px;
+//     top: 6.5px;
+//   }
+//   &_search {
+//     position: absolute;
+//     width: 24px;
+//     height: 24px;
+//     overflow: hidden;
+//     left: 193px;
+//     top: 9.5px;
+//   }
+//   &_user-icon {
+//     position: absolute;
+//     width: 24px;
+//     height: 24px;
+//     overflow: hidden;
+//     left: 240px;
+//     top: 6.5px;
+//   }
+//   &-button {
+//     position: absolute;
+//     width: 80px;
+//     height: 25px;
+//     border: solid #fb7299;
+//     border-radius: 4px;
+//     background-color: #fb7299;
+//     font-size: 13px;
+//     color: #ffff;
+//     left: 275px;
+//     top: 9.5px;
+//   }
+// }
 .head_line {
   position: fixed;
   top: 59px;
---
+++ src/components/top-banner.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="top">
+      <div class="top_left"><img src="static/icon/topIcon.png"
+          alt=""></div>
+      <div class="top_search"><img src="static/icon/find.png"
+          alt=""
+          width="100%"
+          height="100%"></div>
+      <div class="top_user-icon"><img src="static/icon/userIcon.webp"
+          alt=""
+          width="100%"></div>
+      <button class="top-button">下载 App</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'top-banner',
+}
+</script>
+
+<style lang="scss" scoped>
+    .top {
+  position: fixed;
+  top: 0px;
+  z-index: 2500;
+  color: #ffff;
+  &_left {
+    position: absolute;
+    left: 14px;
+    top: 6.5px;
+  }
+  &_search {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    left: 193px;
+    top: 9.5px;
+  }
+  &_user-icon {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    left: 240px;
+    top: 6.5px;
+  }
+  &-button {
+    position: absolute;
+    width: 80px;
+    height: 25px;
+    border: solid #fb7299;
+    border-radius: 4px;
+    background-color: #fb7299;
+    font-size: 13px;
+    color: #ffff;
+    left: 275px;
+    top: 9.5px;
+  }
+}
+</style>
+
