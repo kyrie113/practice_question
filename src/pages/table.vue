af29b4685cc3d7bf69abb48e2d548dec5f9c1444
chenhuigui <chg@spacetech.com.cn>
2019-04-10 18:04:22 +0800

    时间戳

--- src/pages/table.vue
+++ src/pages/table.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
+    <div> {{pickerValue}}</div>
     <img src="static/icon/tableIcon.svg"
       alt="">
     <mt-datetime-picker ref="picker"
       type="time"
-      v-model="pickerValue">
+      v-model="pickerValue"
+      @confirm="handleConfirm"
+      :hourFormat=left>
     </mt-datetime-picker>
+    <div class="close-btn"
+      @click=" openPicker"><img src="static/icon/close.png"
+        alt=""></div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      pickerValue: null,
+      left: 'dsd'
+    }
+  },
+
   methods: {
     openPicker() {
       this.$refs.picker.open()
+    },
+    handleConfirm() {
+      console.log(this.pickerValue)
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 </style>
-
-
